73712198b4913fcd3dfc84d531a14d328edcf67b
Lucy-coding <jie164@icloud.com>
2024-09-25 14:55:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+//typiing
+  const typingText = "Join SKYLER coding club today!";
+  let typingIndex = 0;
+
+  const typingTextElement = document.getElementById('typing-text');
+
+  function typeTypingText() {
+      if (typingIndex < typingText.length) {
+          typingTextElement.textContent += typingText.charAt(typingIndex);
+          typingIndex++;
+          setTimeout(typeTypingText, 100);
+      }
+  }
+
+  typeTypingText();
+
+//汉堡菜单
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+    });
+//报名
+    const signupForm = document.getElementById('signupForm');
+    signupForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const name = e.target[0].value;
+        const age = e.target[1].value;
+        const phone = e.target[2].value;
+
+        const emailBody = `Name: ${name}\nAge: ${age}\nPhone: ${phone}`;
+        window.location.href = `mailto:jie164@icloud.com?subject=Student Registration&body=${encodeURIComponent(emailBody)}`;
+        signupForm.reset();
+    });
+
+//gundong
+
+
+});
